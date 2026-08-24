26b4a6b56a6c9625d6c9cab1861e9533bf082245
domashnii22 <domashnii.al@yandex.ru>
2026-08-24 10:49:27 +0300

    refactor: remove app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
@@ -13,8 +11,8 @@ import { DatabaseModule } from './database/database.module';
     DatabaseModule,
     TasksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
